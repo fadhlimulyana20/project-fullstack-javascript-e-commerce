cefc7855bc9eac4053bc0c493d16df1c6b339ff3
Fadhli Mulyana <baba.fadhli@gmail.com>
2025-08-21 11:11:00 +0700

    menambahkan env untuk production

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -16,5 +16,21 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
-  }
+  },
+  production: {
+    client: process.env.DB_CLIENT || 'mysql2',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'toko-online-db',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
 };
